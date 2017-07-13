c611d64215e3861d453ed540e5fbfa585ca8c640
cre8andctrl <markyap_1@yahoo.com>
2017-07-13 14:43:25 +0800

    asdasd

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/webhook/', function(req, res) {
 				//console.log(result);
 				//qry = query(result)
 				
-				sendText(sender, "Text echos: "+ result)
+				sendText(sender, "Text echos: "+ JSON.stringify(result))
 			});
 			});
 			
