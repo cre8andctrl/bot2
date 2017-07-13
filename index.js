7f61105c8741cf8ef9d32c340771793d92e836af
cre8andctrl <markyap_1@yahoo.com>
2017-07-13 13:30:33 +0800

    test

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM users", function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
+    //console.log(result);
+	res.send(result)
   });
 });
 
